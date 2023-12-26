e61337017e091d425319a8791b582a0b9e09bb5d
NEo0o0 <nathdanai.nepal@gmail.com>
2023-12-26 15:02:40 +0700

    complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<string>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string x[10]={"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    cout<<"You will get "<<x[rand ()%9]<<" in this 261102.";
+
+}
